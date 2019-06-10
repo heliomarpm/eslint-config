50c0781ddecd050cf9a6ffffd77f29b7eddc8ff5
Samuel Meuli <me@samuelmeuli.com>
2019-06-10 12:24:15 +0200

    Allow for-of loops

--- rules/base.js
+++ rules/base.js
@@ -21,6 +21,27 @@ module.exports = {
 		},
 	],
 
+	// Allow for-of loops (but keep other settings for "no-restricted-syntax" rule)
+	// Source: https://github.com/airbnb/javascript/blob/b85baeafed8b66fdd9756439a0b8774860147913/packages/eslint-config-airbnb-base/rules/style.js#L334-L352
+	"no-restricted-syntax": [
+		"error",
+		{
+			selector: "ForInStatement",
+			message:
+				"for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.",
+		},
+		{
+			selector: "LabeledStatement",
+			message:
+				"Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.",
+		},
+		{
+			selector: "WithStatement",
+			message:
+				"`with` is disallowed in strict mode because it makes code impossible to predict and optimize.",
+		},
+	],
+
 	// Import order
 	"import/order": [
 		"error",
